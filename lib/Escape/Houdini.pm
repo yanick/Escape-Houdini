eca14d2d16cdfe757088ed6ba7dd7bf63bf14e86
Yanick Champoux <yanick@cpan.org>
2013-05-14 18:46:06 -0400

    clean up doc

--- lib/Escape/Houdini.pm
+++ lib/Escape/Houdini.pm
@@ -33,7 +33,7 @@ __END__
 =head1 DESCRIPTION
 
 I<Escape::Houdini> is a wrapper around the zero-depedency, minimalistic
-C web escaping library Houdini.
+web escaping C library Houdini.
 
 This version of I<Escape::Houdini> has been built against 
 commit 
@@ -41,28 +41,54 @@ I<e6f0ec96b85578a2fd8ab79af84493cad3a84cfb>
 (Wed Apr 17 17:24:20 2013 +0200) 
 of Houdini.
 
-B<WARNING>: I'm a n00b at XS, so until this module get reviewed by somebody
-who knows what they are doing, take it with a grain of salt.
+=head1 EXPORTS
+
+I<Escape::Houdini> doesn't export any function by default. Functions can be  
+exported individually, or via the tags I<:html> (for I<escape_html> and
+I<unescape_html>), I<:uri> (for I<escape_uri> and I<unescape_uri>),
+I<:url> (for I<escape_url> and I<unescape_url>), I<:js> (for I<escape_js>
+and I<unescape_js>) and I<:all> (for... well, all of them).
+
 
 =func escape_html( $text )
 
+See L<https://github.com/vmg/houdini>
+
 =func unescape_html( $text )
+
+See L<https://github.com/vmg/houdini>
 
 =func escape_xml( $text )
 
+See L<https://github.com/vmg/houdini>
+
 =func escape_uri( $text )
+
+See L<https://github.com/vmg/houdini>
 
 =func unescape_uri( $text )
 
+See L<https://github.com/vmg/houdini>
+
 =func escape_url( $text )
+
+See L<https://github.com/vmg/houdini>
 
 =func unescape_url( $text )
 
+See L<https://github.com/vmg/houdini>
+
 =func escape_href( $text )
+
+See L<https://github.com/vmg/houdini>
 
 =func escape_js( $text )
 
+See L<https://github.com/vmg/houdini>
+
 =func unescape_js( $text )
+
+See L<https://github.com/vmg/houdini>
 
 =head1 SEE ALSO
 
